f5729aab46692939bd32356f35740eab111f4649
Shawn Matthew <smatthew@bnoinc.com>
2015-06-29 16:01:18 -0400

    add files

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+	$("#phone").mask("(999) 999-9999");
+	$("#dob").mask("99/99/9999");
+	$("#ssn").mask("999-99-9999");
+
+	// Display Inputs for Rows w/o subfields
+	$(".form_noSubFields").on("click", function(){
+		
+		$(this).children("label").css("font-size", "16px");		
+		$(this).css({
+			"padding-bottom": "22px",
+			"border": "1px solid #b3d4fc"
+		});
+	});
+
+	// Display Inputs for Rows w/ subfields
+	$(".form_hasSubFields").on("click", function(){
+
+		$(this).children("label").hide();
+		
+		$(this).children(".subField").show();
+		$(this).find("input").css({
+			"border-bottom": "1px solid #ccc"
+		});
+
+		if( $(this).find("input").first().attr("id") == "first" ){
+			$(this).find("input").first().focus();
+		}		
+		
+		$(this).css({
+			"padding-bottom": "0px",
+			"border": "1px solid #b3d4fc"
+		});
+
+	});
+
+
+
+
+});
